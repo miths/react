6d8c123deab7049700f21d09593ba54ef9d50e54
miths <mghinaya@gmail.com>
2020-07-15 02:14:33 +0530

    React Redux Form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { ConfigureStore } from '.redux/ConfigureStore';
+import { ConfigureStore } from './Redux/configureStore';
 
 const store = ConfigureStore();
 
--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-import { Reducer, initialState } form '.Reducer';
+import { Reducer, initialState } from './reducer';
 
 export const ConfigureStore = () => {
     const store = createStore(
